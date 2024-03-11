e90324ea8a8641bbf1b1b074531e1ae5b402acda
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-03-10 21:03:30 -0400

    github link download edit

--- src/components/WindowsDownload.jsx
+++ src/components/WindowsDownload.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WindowsDownload = () => {
     const handleDownload = () => {
-        const downloadUrl = 'https://raw.githubusercontent.com/SharukhGV/dream-journal-bio-distributions/main/src/distributions/dream-journal-bio-1.0.0-setup.exe';
+        const downloadUrl = 'https://github.com/SharukhGV/dream-journal-bio-distributions/raw/main/src/distributions/dream-journal-bio-1.0.0-setup.exe';
         const link = document.createElement('a');
         link.href = downloadUrl;
         link.download = 'dream-journal-bio-1.0.0-setup.exe'; // specify the desired file name
